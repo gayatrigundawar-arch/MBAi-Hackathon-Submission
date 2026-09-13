a711ac5e7b84d567dbd60d63c3a70de40f62bc2e
gayatrigundawar-arch <gayatri.gundawar@gmail.com>
2026-09-13 05:57:10 +0530

    P1

---
+++ countinput.py
@@ -0,0 +1,22 @@
+'''
+Members: Ankur Banga, Yukti Toshniwal
+'''
+
+def countchars(st):
+    cnt = 0 
+    for i in st:
+        if i not in (' ',',','.','!'):
+            cnt+=1
+    return cnt
+
+s = input("Enter a string: ")
+print(countchars(s))
+
+if __name__ == "__main__":
+    assert countchars("Hello world") == 10
+    assert countchars("Hello, world!") == 10
+    assert countchars("Mr. Jones.") == 7
+    assert countchars("1234") == 4
+    assert countchars("Hi?") == 3
+    assert countchars("!!!...,,,") == 0
+    print("All tests passed!")
