8291b1a3a4de89f17c3d7e868a453649e1dccf72
gayatrigundawar-arch <gayatri.gundawar@gmail.com>
2026-09-13 05:52:53 +0530

    Part 1 Stock Trading

---
+++ stocktrading.py
@@ -0,0 +1,22 @@
+'''
+Members: Ankur Banga, Yukti Toshniwal
+'''
+
+def maxProfit(prices):
+    mp = 0
+    ln = prices[0]
+    for i in prices[1:]:
+        if i-ln>mp:
+            mp = i-ln
+        if i<ln:
+            ln = i
+    return mp
+
+if __name__ == "__main__":
+    assert maxProfit([7,1,5,3,6,4]) == 5
+    assert maxProfit([7,6,4,3,1]) == 0
+    assert maxProfit([1,2,3,4,5]) == 4
+    assert maxProfit([3,2,6,5,0,3]) == 4
+    assert maxProfit([1]) == 0
+    print("All tests passed!")
+    
